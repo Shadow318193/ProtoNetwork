65b964f61aaaaaa8c800e43dedd3ef40f21c1ebc
Shadow318193 <shadow318193@gmail.com>
2023-01-29 20:53:55 +0300

    Fix for PythonAnywhere

--- db_backup.py
+++ db_backup.py
@@ -1,7 +1,12 @@
 import os
 import shutil
 
+PYTHONANYWHERE = False
+
 if __name__ == "__main__":
+    if PYTHONANYWHERE:
+        if os.getcwd() == "/home/Shadow318193":
+            os.chdir("/home/Shadow318193/mysite")
     if os.path.isfile("db/social_network.db"):
         shutil.copyfile("db/social_network.db", "db/social_network_backup.db")
         print("Резервная копия базы данных сделана")
